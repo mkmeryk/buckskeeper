bd30810bcb28305813b469583e0b483419416e44
mkmeryk <inq@meryk.ca>
2022-04-29 20:26:48 -0700

    routes setup for controllers

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,44 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index"
   root "welcome#index"
+  get('/flh',{to:'flh#index'})
+  resources :users
+  resource :sessions, only: [:new, :destroy, :create]
+
+  namespace :flh do
+    namespace :resources do
+      resources :posts, controller: 'flh_resources_post' do
+        resources :likes, shallow: true, only: [:create, :destroy], controller: 'flh_resources_likes'
+        get :liked, on: :collection
+      end
+    end
+    namespace :society do
+      resources :questions, controller: 'flh_society_questions' do
+        resources :answers, only:[:create, :destroy], controller: 'flh_society_answers'
+        resources :likes, shallow: true, only: [:create, :destroy], controller: 'flh_society_likes'
+        get :liked, on: :collection
+      end
+    end
+    resources :modules, controller: 'flh_modules'
+    resources :events, controller: 'fel_events'
+     
+  end
+  
+  namespace :personal do
+    resources :income, controller: 'personal_incomes'
+    resources :expenses, controller: 'personal_expenses'
+    resources :savings, controller: 'personal_savings'
+  end
+
+  namespace :business do
+    resources :income, controller: 'business_incomes'
+    resources :expenses, controller: 'business_expenses'
+    resources :savings, controller: 'business_savings'
+  end
+ 
+
+
+ 
+
+
 end
